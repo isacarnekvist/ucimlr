812f7aacd2baa53479a433d219c3406eb8178856
Isac Arnekvist <isac.arnekvist@gmail.com>
2020-04-16 12:19:58 +0200

    bump version, api change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='ucimlr',
-    version='0.2.0',
+    version='0.3.0',
     author='Isac Arnekvist',
     author_email='isac.arnekvist@gmail.com',
     description='Easy access to datasets from the UCI Machine Learning Repository',
